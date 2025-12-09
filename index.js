91ce7b58801c5f9beb3f58627af4ec5711c214b2
Penina <wpenina@gmail.com>
2025-12-09 11:32:10 +0300

    functions practice

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
+function calculateTax(amount) {
+    const taxRate = 0.1;
+    return amount * taxRate;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+function isPalindrome(word) {
+    const reversed = word.split("").reverse().join("");
+    return word === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountPrice = originalPrice - (originalPrice * (discountPercentage / 100));
+    return discountPrice;
+}
 
 
 
